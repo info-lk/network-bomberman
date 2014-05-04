bb1ec478d19005edd2d92cdbbc257f5e25fe4430
Lullatsch <JulianWinter1124@web.de>
2014-05-04 14:25:18 +0200

    Neuerungen
    Kompatiblität zu neuen Server, Vergrößerung der Bandbreite,
    Klasse HasShield?!?! umbenannt :D

--- base/src/network/VPlayer.java
+++ base/src/network/VPlayer.java
@@ -1,23 +1,32 @@
 package network;
 
-import entity.Player;
-
 public class VPlayer {
 
     public double xPosition;
     public double yPosition;
-    public Player.DIRECTION lookDirection;
+    public static enum DIRECTION {UP, DOWN, LEFT, RIGHT}
+    public DIRECTION lookDirection;
+    public int ID;
 
 
     //private double health;
 
     public boolean hasShield;
 
-    public VPlayer(double xPosition, double yPosition, boolean hasShield, Player.DIRECTION lookDirection) {
+    public VPlayer(double xPosition, double yPosition, boolean hasShield, DIRECTION lookDirection, int ID) {
         this.hasShield = hasShield;
         this.xPosition = xPosition;
         this.yPosition = yPosition;
         this.lookDirection = lookDirection;
+        this.ID = ID;
+    }
+
+    public VPlayer(double xPosition, double yPosition, boolean hasShield, int ID) {
+        this.hasShield = hasShield;
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.lookDirection = DIRECTION.DOWN;
+        this.ID = ID;
     }
 
 }
